58dda1f33ffed5e0ce203d7327d83348e61f34de
Konrad Kowalski <rootsher@gmail.com>
2018-08-30 15:58:09 +0200

    Install hot-wire, inject services to actions

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import {
 } from 'connected-react-router';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
+import HotWire from 'hot-wire';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
@@ -20,41 +21,49 @@ import { module as coreModule } from './app/core/core.module';
 import App from './app/routing.component';
 import registerServiceWorker from './registerServiceWorker';
 
-const { rootReducer } = coreModule();
+const { rootReducer, services: coreServices } = coreModule();
 
 const history = createBrowserHistory();
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const middlewares = [
-  routerMiddleware(history),
-  thunkMiddleware.withExtraArgument({}),
-  createLogger() // loggerMiddleware
-];
+const container = new HotWire().wire({
+  services: {
+    ...coreServices
+  }
+});
 
-const store = createStore(
-  connectRouter(history)(rootReducer),
-  composeEnhancer(applyMiddleware(...middlewares))
-);
+container.then(services => {
+  const middlewares = [
+    routerMiddleware(history),
+    thunkMiddleware.withExtraArgument({ services }),
+    createLogger() // loggerMiddleware
+  ];
 
-const render = Component =>
-  ReactDOM.render(
-    <Provider store={store}>
-      <ConnectedRouter history={history}>
-        <Component />
-      </ConnectedRouter>
-    </Provider>,
-    document.getElementById('root')
+  const store = createStore(
+    connectRouter(history)(rootReducer),
+    composeEnhancer(applyMiddleware(...middlewares))
   );
 
-render(App);
+  const render = Component =>
+    ReactDOM.render(
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <Component />
+        </ConnectedRouter>
+      </Provider>,
+      document.getElementById('root')
+    );
 
-if (module.hot) {
-  module.hot.accept('./app/routing.component', () =>
-    render(require('./app/routing.component').default)
-  );
-  module.hot.accept('./app/core/reducers/rootReducer', () => {
-    store.replaceReducer(connectRouter(history)(rootReducer));
-  });
-}
+  render(App);
 
-registerServiceWorker();
+  if (module.hot) {
+    module.hot.accept('./app/routing.component', () =>
+      render(require('./app/routing.component').default)
+    );
+    module.hot.accept('./app/core/reducers/rootReducer', () => {
+      store.replaceReducer(connectRouter(history)(rootReducer));
+    });
+  }
+
+  registerServiceWorker();
+});
